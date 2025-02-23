bedc29c47a4eac1110d8c26a290dc92a034c1248
Faadil Torou <faadil@lynx-business.com>
2025-02-23 15:06:47 +0100

    wip

--- app/Http/Controllers/Organization/Management/UserController.php
+++ app/Http/Controllers/Organization/Management/UserController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Organization\Management;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Function for showing all user
+     *
+     * @return Redirect
+     */
+    public function index()
+    {
+        return Inertia::render('Founder/User/Index');
+    }
 }
